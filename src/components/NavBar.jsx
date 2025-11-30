d58eabdd4109d9ea9c5f6849f77567a9e21a61de
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-11-30 17:40:08 +0100

    feat: navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -86,7 +86,7 @@ const Navbar = () => {
                 >
                     Create Board
                 </Link>
-                
+
             </div>
 
             <div style={styles.spacer} />
